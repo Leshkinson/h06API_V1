e1c0c84e34421dbb7de11b94130b70b588498b61
Oleg Lopatko <vfY2minSRcF9ym7pT>
2023-03-21 16:56:49 +0300

    fixed comment5

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -14,14 +14,12 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
     const tokenService = new TokenService()
     const queryService = new QueryService()
     const payload: string | JwtPayload | JWT = await tokenService.getUserIdByToken(token) as JWT
-    console.log('UserId auth', payload)
     if (!payload) {
         res.sendStatus(401)
 
         return;
     }
     const user = await queryService.findUser(payload.id)
-    console.log('user Auth', user)
 
     if (!user) {
         res.sendStatus(401)
